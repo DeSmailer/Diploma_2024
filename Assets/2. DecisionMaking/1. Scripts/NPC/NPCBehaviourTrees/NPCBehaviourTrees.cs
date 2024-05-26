0389535c1ddec0af310326984ae1d4e2dfa850ec
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2024-05-26 21:20:13 +0300

    update

---
+++ Assets/2. DecisionMaking/1. Scripts/NPC/NPCBehaviourTrees/NPCBehaviourTrees.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace DecisionMaking.BehaviourTrees
+{
+    public class NPCBehaviourTrees : NPC
+    {
+        public override void Initialize(RivalsWarehouse rivalsWarehouse, Farm[] farms, List<ICharacter> characters)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
