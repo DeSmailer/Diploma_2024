218ea395fc03204b8a06f4f8922f67e56291ec10
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2024-05-26 18:25:30 +0300

    update

--- Assets/2. DecisionMaking/1. Scripts/NPC/NPC.cs
+++ Assets/2. DecisionMaking/1. Scripts/NPC/NPC.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
@@ -8,67 +6,50 @@ using UnityUtils;
 
 namespace DecisionMaking
 {
-    public class NPC : MonoBehaviour, IStunned, ICharacter
+    public abstract class NPC : MonoBehaviour, IStunned, ICharacter
     {
-
         [Header("Refarences")]
         [SerializeField] protected NavMeshAgent agent;
-        [SerializeField] Rigidbody rb;
-        [SerializeField] Animator animator;
+        [SerializeField] protected Rigidbody rb;
+        [SerializeField] protected Animator animator;
 
-        [SerializeField] CollisionDetector collisionDetector;
+        [SerializeField] protected CollisionDetector collisionDetector;
 
-        [SerializeField] Inventory inventory;
+        [SerializeField] protected Inventory inventory;
 
         [Header("Speed")]
         [SerializeField] protected float speed = 3.5f;
         [SerializeField] protected float angularSpeed = 120f;
         [SerializeField] protected float stoppingDistance = 0.2f;
 
-        [SerializeField] float stunDuration = 1.5f;
-        [SerializeField] float delayBeforeNewCollision = 0.5f;
+        [SerializeField] protected float stunDuration = 1.5f;
+        [SerializeField] protected float delayBeforeNewCollision = 0.5f;
 
         [SerializeField] protected float viewingRadius = 10f;
         [SerializeField] protected float kamikazeRadius = 5f;
 
-        CountdownTimer stunTimer;
-        StopwatchTimer lastCollisionStopwatchTimer;
+        protected CountdownTimer stunTimer;
+        protected StopwatchTimer lastCollisionStopwatchTimer;
 
-        [SerializeField] float pushForce = 3f;
-        [SerializeField] CharacterInfo characterInfo;
+        [SerializeField] protected float pushForce = 3f;
+        [SerializeField] protected CharacterInfo characterInfo;
 
-        RivalsWarehouse rivalsWarehouse;
-        Farm[] farms;
-        List<ICharacter> otherCharacters;
+        protected RivalsWarehouse rivalsWarehouse;
+        protected Farm[] farms;
+        protected List<ICharacter> otherCharacters;
 
-        StateMachine stateMachine = new();
-
-        NPCRunToFarmState runToFarmState;
-        NPCCollectionState collectionState;
-        NPCRunToWarehouseState runToWarehouseState;
-        NPCGiveAwayResourcesState giveAwayResourcesState;
-        NPCKamikazeState kamikazeState;
-        NPCWanderState wanderState;
-        NPCStunnedState stunnedState;
-
-        public float Speed => speed;
-        public Transform Transform => transform;
-        public Inventory Inventory => inventory;
-        public CharacterInfo CharacterInfo => characterInfo;
+        public virtual float Speed => speed;
+        public virtual Transform Transform => transform;
+        public virtual Inventory Inventory => inventory;
+        public virtual CharacterInfo CharacterInfo => characterInfo;
 
         public UnityEvent OnStunned;
 
-        static readonly int animatorParameterSpeed = Animator.StringToHash("Speed");
+        protected static readonly int animatorParameterSpeed = Animator.StringToHash("Speed");
 
-        public void Initialize(RivalsWarehouse rivalsWarehouse, Farm[] farms, List<ICharacter> characters)
-        {
-            SetupVariables(rivalsWarehouse, farms, characters);
-            SetupTimers();
-            SetupNavMeshAgent();
-            SetupStateMashine();
-        }
+        public abstract void Initialize(RivalsWarehouse rivalsWarehouse, Farm[] farms, List<ICharacter> characters);
 
-        protected void SetupVariables(RivalsWarehouse rivalsWarehouse, Farm[] farms, List<ICharacter> characters)
+        protected virtual void SetupVariables(RivalsWarehouse rivalsWarehouse, Farm[] farms, List<ICharacter> characters)
         {
             this.rivalsWarehouse = rivalsWarehouse;
             this.farms = farms;
@@ -96,65 +77,17 @@ namespace DecisionMaking
             ResumeMovement();
         }
 
-        protected virtual void SetupStateMashine()
-        {
-            stateMachine = new StateMachine();
+        public virtual void ResumeMovement() => agent.isStopped = false;
 
-            runToFarmState = new NPCRunToFarmState(this, animator, agent, farms);
-            collectionState = new NPCCollectionState(this, animator);
+        public virtual void StopMovement() => agent.isStopped = true;
 
-            runToWarehouseState = new NPCRunToWarehouseState(this, animator, agent, rivalsWarehouse);
-            giveAwayResourcesState = new NPCGiveAwayResourcesState(this, animator);
-
-            kamikazeState = new NPCKamikazeState(this, animator, otherCharacters, agent, transform, kamikazeRadius);
-
-            wanderState = new NPCWanderState(this, animator, agent, viewingRadius);
-            stunnedState = new NPCStunnedState(this, animator);
-
-
-            stateMachine.AddTransition(kamikazeState, runToFarmState, new FuncPredicate(() => kamikazeState.TargetNotFound));
-            stateMachine.AddTransition(giveAwayResourcesState, kamikazeState, new FuncPredicate(() => runToWarehouseState.IsComplete && CharactersInRadius()));
-            stateMachine.AddTransition(giveAwayResourcesState, runToFarmState, new FuncPredicate(() => runToWarehouseState.IsComplete && !CharactersInRadius()));
-            stateMachine.AddTransition(runToWarehouseState, giveAwayResourcesState, new FuncPredicate(() => runToWarehouseState.IsComplete));
-            stateMachine.AddTransition(collectionState, runToWarehouseState, new FuncPredicate(() => collectionState.IsComplete && inventory.FillPercentage >= 0.7));
-            stateMachine.AddTransition(collectionState, runToFarmState, new FuncPredicate(() => collectionState.IsComplete && inventory.FillPercentage < 0.7));
-            stateMachine.AddTransition(runToFarmState, collectionState, new FuncPredicate(() => runToFarmState.IsComplete));
-            stateMachine.AddTransition(wanderState, runToFarmState, new FuncPredicate(() => wanderState.IsComplete && inventory.FillPercentage < 50));
-            stateMachine.AddTransition(stunnedState, wanderState, new FuncPredicate(() => stunTimer.IsFinished));
-
-            stateMachine.AddTransition(kamikazeState, stunnedState,
-                new FuncPredicate(() => collisionDetector.IsDetected && lastCollisionStopwatchTimer.GetTime() > delayBeforeNewCollision));
-            stateMachine.AddAnyTransition(stunnedState,
-               new FuncPredicate(() => collisionDetector.IsDetected && lastCollisionStopwatchTimer.GetTime() > delayBeforeNewCollision));
-
-
-            stateMachine.SetState(wanderState);
-        }
-
-        void Update()
-        {
-            stateMachine.Update();
-            stunTimer.Tick(Time.deltaTime);
-            lastCollisionStopwatchTimer.Tick(Time.deltaTime);
-            UpdadeteAnimator();
-        }
-
-        void FixedUpdate()
-        {
-            stateMachine.FixedUpdate();
-        }
-
-        public void ResumeMovement() => agent.isStopped = false;
-
-        public void StopMovement() => agent.isStopped = true;
-
-        void UpdadeteAnimator()
+        protected virtual void UpdadeteAnimator()
         {
             animator.SetFloat(animatorParameterSpeed, Speed);
         }
 
 
-        public void Stun(float duration = 1)
+        public virtual void Stun(float duration = 1)
         {
             stunTimer.Start();
             inventory.DropOnGround();
@@ -162,7 +95,7 @@ namespace DecisionMaking
             OnStunned?.Invoke();
         }
 
-        protected bool CharactersInRadius()
+        protected virtual bool CharactersInRadius()
         {
             foreach(var character in otherCharacters)
             {
@@ -174,7 +107,7 @@ namespace DecisionMaking
             return false;
         }
 
-        public void StopAllForces()
+        public virtual void StopAllForces()
         {
             rb.velocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
--- Assets/2. DecisionMaking/1. Scripts/NPC/NPCStateMashine/NPCStateMashine.cs
+++ Assets/2. DecisionMaking/1. Scripts/NPC/NPCStateMashine/NPCStateMashine.cs
@@ -1,18 +1,84 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityUtils;
 
-public class NPCStateMashine : MonoBehaviour
+namespace DecisionMaking
 {
-    // Start is called before the first frame update
-    void Start()
+    public class NPCStateMashine : NPC
     {
-        
-    }
+        protected StateMachine stateMachine = new();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        protected NPCRunToFarmState runToFarmState;
+        protected NPCCollectionState collectionState;
+        protected NPCRunToWarehouseState runToWarehouseState;
+        protected NPCGiveAwayResourcesState giveAwayResourcesState;
+        protected NPCKamikazeState kamikazeState;
+        protected NPCWanderState wanderState;
+        protected NPCStunnedState stunnedState;
+
+        public override void Initialize(RivalsWarehouse rivalsWarehouse, Farm[] farms, List<ICharacter> characters)
+        {
+            SetupVariables(rivalsWarehouse, farms, characters);
+            SetupTimers();
+            SetupNavMeshAgent();
+            SetupStateMashine();
+        }
+
+        protected virtual void SetupStateMashine()
+        {
+            stateMachine = new StateMachine();
+
+            runToFarmState = new NPCRunToFarmState(this, animator, agent, farms);
+            collectionState = new NPCCollectionState(this, animator);
+
+            runToWarehouseState = new NPCRunToWarehouseState(this, animator, agent, rivalsWarehouse);
+            giveAwayResourcesState = new NPCGiveAwayResourcesState(this, animator);
+
+            kamikazeState = new NPCKamikazeState(this, animator, otherCharacters, agent, transform, kamikazeRadius);
+
+            wanderState = new NPCWanderState(this, animator, agent, viewingRadius);
+            stunnedState = new NPCStunnedState(this, animator);
+
+
+            stateMachine.AddTransition(kamikazeState, runToFarmState, new FuncPredicate(() => kamikazeState.TargetNotFound));
+            stateMachine.AddTransition(giveAwayResourcesState, kamikazeState, new FuncPredicate(() => runToWarehouseState.IsComplete && CharactersInRadius()));
+            stateMachine.AddTransition(giveAwayResourcesState, runToFarmState, new FuncPredicate(() => runToWarehouseState.IsComplete && !CharactersInRadius()));
+            stateMachine.AddTransition(runToWarehouseState, giveAwayResourcesState, new FuncPredicate(() => runToWarehouseState.IsComplete));
+            stateMachine.AddTransition(collectionState, runToWarehouseState, new FuncPredicate(() => collectionState.IsComplete && inventory.FillPercentage >= 0.7));
+            stateMachine.AddTransition(collectionState, runToFarmState, new FuncPredicate(() => collectionState.IsComplete && inventory.FillPercentage < 0.7));
+            stateMachine.AddTransition(runToFarmState, collectionState, new FuncPredicate(() => runToFarmState.IsComplete));
+            stateMachine.AddTransition(wanderState, runToFarmState, new FuncPredicate(() => wanderState.IsComplete && inventory.FillPercentage < 50));
+            stateMachine.AddTransition(stunnedState, wanderState, new FuncPredicate(() => stunTimer.IsFinished));
+
+            stateMachine.AddTransition(kamikazeState, stunnedState,
+                new FuncPredicate(() => collisionDetector.IsDetected && lastCollisionStopwatchTimer.GetTime() > delayBeforeNewCollision));
+            stateMachine.AddAnyTransition(stunnedState,
+               new FuncPredicate(() => collisionDetector.IsDetected && lastCollisionStopwatchTimer.GetTime() > delayBeforeNewCollision));
+
+
+            stateMachine.SetState(wanderState);
+        }
+
+        void Update()
+        {
+            stateMachine.Update();
+            stunTimer.Tick(Time.deltaTime);
+            lastCollisionStopwatchTimer.Tick(Time.deltaTime);
+            UpdadeteAnimator();
+        }
+
+        void FixedUpdate()
+        {
+            stateMachine.FixedUpdate();
+        }
+
+        protected override void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, viewingRadius);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, kamikazeRadius);
+        }
     }
 }
