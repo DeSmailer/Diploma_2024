c2b21cbc94b3c551d6e3e828d6ed36dd00f3a1b7
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2024-05-26 23:56:05 +0300

    MBT

---
+++ Assets/2. DecisionMaking/1. Scripts/NPC/NPCBehaviourTrees/UntilFail.cs
@@ -0,0 +1,43 @@
+﻿namespace DecisionMaking.BehaviorTree
+{
+    public class UntilFail : Node
+    {
+        public UntilFail(string name) : base(name) { }
+
+        public override Status Process()
+        {
+            if(children[0].Process() == Status.Failure)
+            {
+                Reset();
+                return Status.Failure;
+            }
+            return Status.Running;
+        }
+    }
+
+
+}
+//public class RandomSelector : Node
+//{
+//    public RandomSelector(string name, int priority = 0) : base(name, priority) { }
+
+//    public override Status Process()
+//    {
+//        if(currentChild < children.Count)
+//        {
+//            switch(children[currentChild].Process())
+//            {
+//                case Status.Running:
+//                    return Status.Running;
+//                case Status.Success:
+//                    Reset();
+//                    return Status.Success;
+//                default:
+//                    currentChild++;
+//                    return Status.Running;
+//            }
+//        }
+//        Reset();
+//        return Status.Failure;
+//    }
+//}
